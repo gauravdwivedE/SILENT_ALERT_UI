cd47bab9b50f28af24c7624a3c2ece97589343ff
Gaurav Dwivedi <168353151+gauravdwivedE@users.noreply.github.com>
2025-07-03 14:21:02 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api/',
+    baseURL: `${import.meta.env.VITE_GOOGLE_CLIENT_ID}`,
     timeout: 60000,
    
 })
